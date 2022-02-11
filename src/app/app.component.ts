49bdf1ae70d2a97d7a1617190be9397fab5af672
LHS2S6 <poczospeti@gmail.com>
2022-02-11 14:21:06 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  title = "zsiraf"
+
+  items = ['item1', 'item2', 'item3', 'item4'];
+
+  people =[
+    {name: 'Aladar', age: 36, color: 'red'},
+    {name: 'Aladar2', age: 35, color: 'green'},
+    {name: 'Aladar3', age: 37, color: 'blue'},
+
+  ];
+
+  removeLastElement() {
+    this.items.pop();
+  }
 }
